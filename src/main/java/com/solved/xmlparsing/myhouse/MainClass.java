eac591a91be3c1687b1c1bde327bbef06b74a960
Ruslan <92981991+russtolyar@users.noreply.github.com>
2021-11-13 13:47:26 +0300

    Hw  013 (#4)
    * corrected
    
    * corrected//
    
    * Parser StAX and Parser Jaxb Complieted
    
    Co-authored-by: rus-sto <rus-sto@tut.by>

--- src/main/java/com/solved/xmlparsing/myhouse/MainClass.java
+++ src/main/java/com/solved/xmlparsing/myhouse/MainClass.java
@@ -4,12 +4,15 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        MySTAXParserImpl mySTAXParser = new MySTAXParserImpl();
-
         String fileName = "src/main/resources/house.xml";
 
+        System.out.println("\t\t\tStAX parser\n");
+        MySTAXParserImpl mySTAXParser = new MySTAXParserImpl();
         System.out.println(mySTAXParser.parse(fileName));
 
+        System.out.println("\n\n\t\t\tJaxb parser\n");
+        MyJaxbParserImpl myJaxbParserImpl = new MyJaxbParserImpl();
+        System.out.println(myJaxbParserImpl.parse(fileName));
 
     }
 }
